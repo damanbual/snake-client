8e980605d36d306ad83d56db45e05b0737d0fc01
Daman Bual <damanbual@gmail.com>
2024-06-13 11:37:07 -0700

    Add connection event handler and send name to server

--- client.js
+++ client.js
@@ -1,28 +1,29 @@
 
 const net = require("net");
 
-// establishes a connection with the game server
+// Establishes a connection with the game server
 const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here
-    port: 50541, // PORT number here
-  });
-  
-  // interpret incoming data as text
+  const conn = net.createConnection(
+    {
+      host: "localhost", // IP address here
+      port: 50541, // PORT number here
+    },
+    () => {
+      // 'connect' listener
+      console.log("Successfully connected to game server");
+      conn.write("Name: DB"); // Send the client's name to the server
+    }
+  );
+
+  // Interpret incoming data as text
   conn.setEncoding("utf8");
-  
+
   // Handle incoming data from the server
   conn.on("data", (data) => {
     console.log("Server says:", data);
   });
-  
-  // Handle connection event
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-  });
-  
+
   return conn;
 };
 
-// Export the connect function
 module.exports = { connect };
