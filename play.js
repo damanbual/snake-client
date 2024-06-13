b3952f816507b4a8b6984075e5a197e0f460e600
Daman Bual <damanbual@gmail.com>
2024-06-13 11:27:42 -0700

    Modularize connection logic

--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-const net = require("net");
-// establishes a connection with the game server
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here
-    port: 50541, // PORT number here
-  });
-  
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  // Handle incoming data from the server
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
-  
-  // Handle connection event
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-  });
-  return conn;
-};
+// Import the connect function from client module
+const { connect } = require("./client");
 
 console.log("Connecting ...");
+// Call the connect function
 connect();
