66885e9cfc40084e2e09e53cacbfda01923b9cfb
Daman Bual <damanbual@gmail.com>
2024-06-13 11:16:28 -0700

    Add connection setup for Snake client

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+// establishes a connection with the game server
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here
+    port: 50541, // PORT number here
+  });
+  
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  // Handle incoming data from the server
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+  
+  // Handle connection event
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
